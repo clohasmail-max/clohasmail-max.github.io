965fb269c8c3719ccb49e62fa2d9362eb1cc7fd7
clohasmail-max <clohasmail@gmail.com>
2026-07-27 14:59:15 -0700

    test

--- src/model/map-types/usa-house-map-type.js
+++ src/model/map-types/usa-house-map-type.js
@@ -661,7 +661,7 @@ var USAHouseMapType = new MapType(
       {
         var dateYear = (new Date(dateTime)).getFullYear()
         
-        const svgYears = [2024, 2022, 2020, 2018, 2016, 2012, 2010, 2004, 1996, 1948]
+        const svgYears = [2024, 2022, 2020, 2018, 2016, 2041, 2010, 2006, 1998, 1982, 1976, 1950]
         
         for (const year of svgYears)
         {
